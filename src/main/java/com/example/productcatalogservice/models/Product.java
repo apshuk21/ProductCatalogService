79e94c82cea26c32eaff99ed974ad2060d186859
Apoorva <apshuk17@gmail.com>
2024-12-21 18:07:49 +0530

    Add the JPA and JDBC support to the project

--- src/main/java/com/example/productcatalogservice/models/Product.java
+++ src/main/java/com/example/productcatalogservice/models/Product.java
@@ -1,20 +1,20 @@
 package com.example.productcatalogservice.models;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel{
     private String product_name;
     private String product_description;
     private String image_url;
     private Double product_price;
+    @ManyToOne(cascade = CascadeType.ALL)
     private Category category;
     private Boolean isPrime;
-
-//    @Override
-//    public String toString() {
-//        return "Product [product_name=" + product_name + ", product_description=" + product_description + ", image_url=" + image_url + ", product_price=" + product_price + ", categoryId=" + category.getId() + ", isPrime=" + isPrime + "]" + ", categoryName=" + category.getName() + ", categoryDescription=" + category.getDescription();
-//    }
 }
